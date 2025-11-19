c0ada15f6f16886fa72e6687c2ad58de9bd3a7be
Eight <malupauluci@gmail.com>
2025-11-19 19:53:20 -0300

    Atividade final

---
+++ Projeto Visual Studio/Atividade8 API/Atividade8 API/Controllers/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using Atividade8_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade8_API.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public static List<Jogador> Jogadores = new()
+        {
+            new Jogador { id = 1, Vida = 20, QuantidadeDeItens = 3, PosicaoX = 0, PosicaoY = 0, PosicaoZ = 0 },
+            new Jogador { id = 2, Vida = 20, QuantidadeDeItens = 3, PosicaoX = 1, PosicaoY = 1, PosicaoZ = 1 }
+        };
+
+        [HttpGet]
+        [Route("api/jogador")]
+        public IActionResult GetJogadores()
+        {
+            return Ok(Jogadores);
+        }
+
+        [HttpGet]
+        [Route("api/jogador/id")]
+        public IActionResult GetJogador(int id)
+        {
+            var jogador = Jogadores.FirstOrDefault(a => a.id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+            return Ok(jogador);
+        }
+
+
+        [HttpPost]
+        [Route("api/jogador")]
+        public IActionResult AddJogador([FromBody] Jogador novoJogador)
+        {
+            Jogadores.Add(novoJogador);
+            return Ok(novoJogador);
+        }
+        [HttpPut]
+        [Route("api/jogador/{id}")]
+        public IActionResult UpdateJogador(int id, [FromBody] Jogador jogadorAtualizado)
+        {
+            var jogador = Jogadores.FirstOrDefault(a => a.id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+            jogador.Vida = jogadorAtualizado.Vida;
+            jogador.QuantidadeDeItens = jogadorAtualizado.QuantidadeDeItens;
+            jogador.PosicaoX = jogadorAtualizado.PosicaoX;
+            jogador.PosicaoY = jogadorAtualizado.PosicaoY;
+            jogador.PosicaoZ = jogadorAtualizado.PosicaoZ;
+            return Ok(jogador);
+        }
+        [HttpDelete]
+        [Route("api/jogador/{id}")]
+        public IActionResult DeleteJogador(int id)
+        {
+            var jogador = Jogadores.FirstOrDefault(a => a.id == id);
+            if (jogador == null)
+            {
+                return NotFound();
+            }
+            Jogadores.Remove(jogador);
+            return Ok();
+        }
+    }
+}
---
+++ Projeto Visual Studio/Atividade8 API/Atividade8 API/Models/Jogador.cs
@@ -0,0 +1,12 @@
+﻿namespace Atividade8_API.Models
+{
+    public class Jogador
+    {
+        public int id {  get; set; }
+        public int Vida {  get; set; }
+        public int QuantidadeDeItens { get; set; }
+        public int PosicaoX { get; set; }
+        public int PosicaoY { get; set; }
+        public int PosicaoZ { get; set; }
+    }
+}
